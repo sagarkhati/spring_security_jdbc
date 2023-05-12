88674063abe729a26b329da191cf61c91b8b3e79
Sagar Singh Khati <sonu06sagar@gmail.com>
2023-05-12 14:07:09 +0530

    added query to fetch user and roles from custom schemas

--- src/main/java/com/ssk/spring_security_jdbc/SecurityConfigWithoutHardcodedUsers.java
+++ src/main/java/com/ssk/spring_security_jdbc/SecurityConfigWithoutHardcodedUsers.java
@@ -19,7 +19,9 @@ public class SecurityConfigWithoutHardcodedUsers extends WebSecurityConfigurerAd
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.jdbcAuthentication().dataSource(dataSource);
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery("select username, password, enabled from users where username = ?")
+				.authoritiesByUsernameQuery("select username, authority from authorities where username = ?");
 	}
 
 	@Override
