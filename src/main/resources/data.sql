f0f6b2fb39c1f1706ca7d721fc5d065db143b0a6
Sagar Singh Khati <sonu06sagar@gmail.com>
2023-05-13 14:50:19 +0530

    jdbcAuth with Bcrypt

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,8 @@
-INSERT INTO users (username, password, enabled) values ('user', 'pass', true);
-INSERT INTO users (username, password, enabled) values ('admin', 'pass', true);
+--INSERT INTO users (username, password, enabled) values ('user', 'pass', true);
+--INSERT INTO users (username, password, enabled) values ('admin', 'pass', true);
+INSERT INTO users (username, password, enabled) values ('user', '$2a$10$gkYoXVcDuQlq8xiSe5yB2eC3r7csQd4yOj6keV47ev0lQ7Ax47ROm', true);
+INSERT INTO users (username, password, enabled) values ('admin', '$2a$10$gkYoXVcDuQlq8xiSe5yB2eC3r7csQd4yOj6keV47ev0lQ7Ax47ROm', true);
+
 
 INSERT INTO authorities (username, authority) values ('user', 'ROLE_USER');
 INSERT INTO authorities (username, authority) values ('admin', 'ROLE_ADMIN');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 create table users(
 	username varchar_ignorecase(50) not null primary key,
-	password varchar_ignorecase(50) not null,
+	password varchar_ignorecase(60) not null,
 	enabled boolean not null
 );
 
